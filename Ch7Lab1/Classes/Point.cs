cbb4e8615789a6e4587dd337d226431db22bb3e4
Brittany Pruneau <69804752+BrittanyPruneau@users.noreply.github.com>
2021-08-21 15:47:35 -0600

    Add files via upload

---
+++ Ch7Lab1/Classes/Point.cs
@@ -0,0 +1,46 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        private int x, y; // here we added two private fields, x and y AND we will modify the constructors of these fields. 
+        public Point()   //  here we have the default constructor because we arent passing anything in it. 
+                        //The program will build with this. You only need to write a default constructor IF you have overloaded the other constructor. 
+        {
+            Console.WriteLine("Default constructor called");
+            this.x = -1;
+            this.y = -1; 
+        }
+        
+        // The following is a public constructor that accepts two int arguments called x and y, that called 
+        //the Console.WriteLine method to display the values of these arguments to the console.
+        public Point(int x, int y)
+        {
+            this.x = x;   // this keyword    means " the field in this object (Point). 
+            this.y = y; // this keyword is telling the compiler which variables are parameters and whcih 
+                        // variables are fields ! 
+
+            // // just this code alone will not compile(before I added more in, because we havent passed x and y into the Constructor from any Object yet. 
+            // IS THIS CORRECT  -> above statement   ???????????
+            // rather, the call to the DEFAULT constructor in the doWork Method (Point object is inside this method), is now invalid bc 
+            //there is no longer a default constructor since we just overloaded it with two integers. 
+        }
+
+        public double DistanceTo(Point other)
+        {
+            int xDiff = this.x - other.x;  // although x and y are private fields, different instances of the same class can access them. 
+            int yDiff = this.y - other.y;    // this.y is a field  other.y is a field. But PRIVATE operates at the class level.  
+            double distance = Math.Sqrt((xDiff * xDiff) + (yDiff * yDiff)); // looks like we can use the Math class because
+            return distance; 
+        
+        }                                                                   // it is in the System NameSpace.  
+
+    }  
+}
---
+++ Ch7Lab1/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Point origin = new Point(); // this statement creates a new instance of the Point class and invokes its default constructor.  
+            
+            Point bottomRight = new Point(1366, 768); // declared bottomRight variable of type Point. 
+            // initialized it to a new Point object by using the constructor with two arguments and supplied the values 1366 and 768.
+
+
+        }   // we CAN build from here, because the compiler generates an automatic constructor for the Point class(but its hidden). 
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
